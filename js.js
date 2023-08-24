a4ba4de6f29336cd603fe997a151fc7b37a004f2
“Emma <emmalarsen2704@gmail.com>
2023-08-24 11:01:20 +0200

    tilføjet foto

--- js.js
+++ js.js
@@ -7,3 +7,11 @@ info.textContent += "YAYYY";
 
 console.log ("virker det?")
 nummer2.textContent ="hallo"
+
+const foto1 = document.querySelector ("#foto1")
+console.log ("virker det foto?")
+foto1.src ="https://picsum.photos/id/2/200/300"
+
+const foto2 = document.querySelector ("#foto2")
+console.log ("virker det foto?")
+foto2.src ="https://picsum.photos/id/21/200/300"
