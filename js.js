94cf7f69ccc71b3c70710bdb9983844245fc051a
“Emma <emmalarsen2704@gmail.com>
2023-08-24 10:47:47 +0200

    tilføjet to const

---
+++ js.js
@@ -0,0 +1,9 @@
+const info = document.querySelector("#info");
+const nummer2 = document.querySelector("#undertitel")
+
+console.log ("consolebaby");
+info.textContent += "YAYYY";
+
+
+console.log ("virker det?")
+nummer2.textContent ="hallo"
